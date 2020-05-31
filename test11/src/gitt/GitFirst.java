638e8496c4f7012430df4ef06fb17cd28ba48cb6
mingz <669326977@qq.com>
2020-05-31 15:51:12 +0800

    第一次提交

---
+++ test11/src/gitt/GitFirst.java
@@ -0,0 +1,12 @@
+package gitt;
+
+public class GitFirst {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+    }
+}
